4b50a0f9dd95d8066d88da96dbc72c5c0068a92a
Jiwant <jiwantsingh01@gmail.com>
2018-05-28 14:37:53 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+
+//Jiwant Singh
+//Nihal Ahmed Gesudraz
+//Apoorva Solanki
+//Kiranpreet Kaur
+//Harkirat Kaur
+
+namespace RockPaperScissor
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		RPSVM vm = new RPSVM();
+		const int rock = 1;
+		const int paper = 2;
+		const int scissor = 3;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = vm;
+			vm.ComputerMakesSelection();
+		}
+
+		private void Replay(object sender, EventArgs e)
+		{
+			vm.Replay();
+		}
+
+		private void Rock(object sender, EventArgs e)
+		{
+			vm.GamePlay(rock);
+		}
+
+		private void Paper(object sender, EventArgs e)
+		{
+			vm.GamePlay(paper);
+		}
+
+		private void Scissor(object sender, EventArgs e)
+		{
+			vm.GamePlay(scissor);
+		}
+	}
+}
---
+++ RPSVM.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RockPaperScissor
+{
+    class RPSVM : INotifyPropertyChanged
+	{
+		const int unsel = 0;
+		const int rock = 1;
+		const int paper = 2;
+		const int scissor = 3;
+
+		int usrSel=unsel;
+		int compSel=unsel;
+		bool vis = false;
+		bool enable = true;
+		string src;
+		string msg="Play the Game";
+
+		public bool Vis
+		{
+			get{ return vis ; }
+			set{ vis = value; OnPropertyChanged(); }
+		}
+
+		public bool Enable
+		{
+			get { return enable; }
+			set { enable = value; OnPropertyChanged(); }
+		}
+		public string Src
+		{
+			get { return src; }
+			set { src = value; OnPropertyChanged(); }
+		}
+
+		public string Msg
+		{
+			get { return msg; }
+			set { msg = value; OnPropertyChanged(); }
+		}
+
+		public void ComputerMakesSelection()
+		{
+			Random rnd = new Random();
+			compSel = rnd.Next(rock, scissor+1);
+			if (compSel == rock)
+				Src = "Rock.bmp";
+			else if (compSel == paper)
+				Src = "Paper.bmp";
+			else if (compSel == scissor)
+				Src = "Scissors.bmp";
+		}
+
+		public void GamePlay(int selection)
+		{
+			Enable = false;
+			usrSel = selection;
+			Vis = true;
+			string UserWin = "Congratulation, You Win";
+			string ComputerWin = "You Loose";
+
+			if (usrSel == compSel)
+				Msg = "Match is Draw, Try Again";
+			else
+			{
+				if(usrSel==rock)
+				{
+					if(compSel==paper)
+					{
+						Msg = ComputerWin;
+					}
+
+					else if(compSel==scissor)
+					{
+						Msg = UserWin;
+					}
+				}
+				else if(usrSel==paper)
+				{
+					if(compSel==rock)
+					{
+						Msg = UserWin;
+					}
+
+					else if(compSel==scissor)
+					{
+						Msg = ComputerWin;
+					}
+				}
+				else if(usrSel==scissor)
+				{
+					if(compSel==rock)
+					{
+						Msg = ComputerWin;
+					}
+
+					else if(compSel==paper)
+					{
+						Msg = UserWin;
+					}
+				}
+			}
+		}
+
+		public void Replay()
+		{
+			Enable = true;
+			usrSel = unsel;
+			compSel = unsel;
+			Vis = false;
+			Src = "No Selection";
+			Msg = "Play the Game";
+			ComputerMakesSelection();
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged([CallerMemberName] String propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
